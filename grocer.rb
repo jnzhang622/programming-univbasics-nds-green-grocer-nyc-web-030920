1a3e3bf3ebe53a66031375248581b9ecd890fce8
John Zhang <jnzhang622@gmail.com>
2020-02-12 08:58:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -87,7 +87,7 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   
-  pp cart
+
   
   new_cart = consolidate_cart(cart)
   apply_coupons(new_cart, coupons)
